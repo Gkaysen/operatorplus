00b453a99740b502f0f0bdd1da44040a989ac3a9
ciecclm <ciecclm@imnu.edu.cn>
2018-05-28 09:25:52 +0800

    Initial commit

---
+++ operatorplus/operatorplus.cpp
@@ -0,0 +1,48 @@
+// operatorplus.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+class CFeet
+{
+private:
+	int feet;
+	int inches;
+public:
+	void setvalue(int fe, int in);
+	void display();
+	CFeet add(CFeet & objf);
+	CFeet operator +(CFeet & objf);
+};
+void CFeet::setvalue(int fe, int in)
+{
+	feet = fe+in/12;
+	inches = in%12;
+}
+void CFeet::display()
+{
+	cout << feet << "英尺" << inches << "英寸" << endl;
+}
+CFeet CFeet::add(CFeet & objf)
+{
+	CFeet temp;
+	temp.setvalue(feet + objf.feet, inches + objf.inches);
+	return temp;
+}
+CFeet CFeet::operator +(CFeet & objf)
+{
+	CFeet temp;
+	temp.setvalue(feet + objf.feet, inches + objf.inches);
+	return temp;
+}
+int main()
+{
+	CFeet A,B,C;
+	A.setvalue(10,11);
+	B.setvalue(6, 9);
+	C = A + B;
+	C.display();
+    return 0;
+}
+
